2c362ad947d8c79518f6f48a7ea35333c0c8f42a
David Silverio <2190730@my.ipleiria.pt>
2020-12-18 17:54:38 +0000

    Modificadas as views de criar e atualizar requisições

--- backend/views/requisicao/update.php
+++ backend/views/requisicao/update.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Requisicao */
@@ -14,8 +15,29 @@ $this->params['breadcrumbs'][] = 'Update';
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <div class="rowStyling rowAddBook">
+        <div class="row">
+            <?php $form = ActiveForm::begin(); ?>
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <?= $form->field($model, 'dta_levantamento')->label('Data de levantamento')->input('date') ?>
+                <?= $form->field($model, 'dta_entrega')->label('Data de entrega')->input('date') ?>
+                <?= $form->field($model, 'estado')->textInput() ?>
+                <?= $form->field($model, 'id_utilizador')->textInput() ?>
+                <?= $form->field($model, 'id_bib_levantamento')->textInput() ?>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary']) ?>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <?php ActiveForm::end(); ?>
+    </div>
 
 </div>
