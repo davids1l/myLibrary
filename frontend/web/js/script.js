e74e7c58b8af163600ccd5bc174a61a2bf4c4cff
David Silvério <2190730@my.ipleiria.pt>
2021-01-04 16:56:02 +0000

    subscribe com o client js a funcionar

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -1,46 +1,3 @@
-/*var client;
-var reconnectTimeout = 2000;
-var host="127.0.0.1";
-var port=9001;
-
-function onConnect() {
-    console.log("connected sucessfully");
-    client.subscribe("req/#");
-    message = new Paho.MQTT.Message("Hello");
-    message.destinationName = "req/hi";
-    client.send(message);
-}
-function onFailure(message) {
-    console.log("connection attempt to host " + host + " failed");
-    //setTimeout(MQTTConnect, reconnectTimeout);
-}
-function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-        console.log("onConnectionLost:"+responseObject.errorMessage);
-    }
-}
-function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-}
-
-function MQTTConnect() {
-    console.log("connecting to " + host + " " + port);
-    client = new Paho.MQTT.Client(host, port, "pahomqttSubscriber");
-    var options = {
-        timeout: 3,
-        onSuccess: onConnect,
-        mqttVersion: 3
-        //onFailure: onFailure
-    };
-    client.onMessageArrived = onMessageArrived;
-    client.onConnectionLost = onConnectionLost;
-
-    client.connect(options);
-}
-
-MQTTConnect();*/
-
-
 var options = {
     clean: true, // retain session
     connectTimeout: 4000, // Timeout period
@@ -63,7 +20,7 @@ client.on('error', function(error){
 
 client.on('message', function(topic, message){
     console.log('receive message：', topic, message.toString())
-    client.end()
+    //client.end()
 })
 
 client.publish("req/test1", "hello from local!")
