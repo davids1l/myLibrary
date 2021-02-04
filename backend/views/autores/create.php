4e0782e545a32d43e6989a16c79cd01337dea39f
Tiago Loeps <2190735@my.ipleiria.pt>
2021-02-04 23:33:10 +0000

    Redesign das editoras e dos autores

--- backend/views/autores/create.php
+++ backend/views/autores/create.php
@@ -12,17 +12,18 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="autor-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="topicos"><?= Html::encode($this->title) ?></h1>
+    <hr>
 
-    <div class="row rowStyling">
+    <div class="row">
         <?php $form = ActiveForm::begin(); ?>
         <div class="col-md-4"></div>
         <div class="col-md-4">
-            <?= $form->field($model, 'nome_autor')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'nome_autor')->textInput(['maxlength' => true, 'placeholder' => 'introduza o nome do autor'])->label('Nome do Autor') ?>
             <?= $form->field($model, 'id_pais')->dropDownList($paises)->label('País') ?>
 
-            <div class="form-group center">
-                <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary']) ?>
+            <div class="form-group text-center">
+                <?= Html::submitButton('<i class="fas fa-plus"></i> Adicionar', ['class' => 'btnAcao']) ?>
             </div>
 
         </div>
--- backend/views/autores/update.php
+++ backend/views/autores/update.php
@@ -6,24 +6,25 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\Models\Autor */
 
-$this->title = 'Atualizar Autor: ' . $model->id_autor;
+$this->title = 'Editar Autor: ' . $model->nome_autor;
 $this->params['breadcrumbs'][] = ['label' => 'Autores', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id_autor, 'url' => ['view', 'id' => $model->id_autor]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = ['label' => $model->nome_autor];
+$this->params['breadcrumbs'][] = 'Editar';
 ?>
 <div class="autor-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="topicos"><?= Html::encode($this->title) ?></h1>
+    <hr>
 
-    <div class="row rowStyling">
+    <div class="row">
         <?php $form = ActiveForm::begin(); ?>
         <div class="col-md-4"></div>
         <div class="col-md-4">
-            <?= $form->field($model, 'nome_autor')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'nome_autor')->textInput(['maxlength' => true])->label('Nome do Autor') ?>
             <?= $form->field($model, 'id_pais')->dropDownList($paises)->label('País') ?>
 
-            <div class="form-group center">
-                <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary']) ?>
+            <div class="form-group text-center">
+                <?= Html::submitButton('<i class="fas fa-save"></i> Guardar', ['class' => 'btnAcao']) ?>
             </div>
 
         </div>
